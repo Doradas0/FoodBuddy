2c8bb7fd6641dcecb37359094eedf907403ce2f6
DMALCOL <daniel.malcolm@daimler.com>
2019-10-04 16:18:52 +0100

    added menu plan container

---
+++ src/containers/MenuPlan/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return(
+    <div className="MenuPlan">
+      <h1>Menu Plan</h1>
+    </div>
+  )
+}
--- src/utils/Routes.js
+++ src/utils/Routes.js
@@ -3,11 +3,14 @@ import { Route, Switch } from "react-router-dom";
 import NotFound from '../containers/NotFound';
 import Home from "../containers/Home";
 import RecipeBook from "../containers/RecipeBook";
+import MenuPlan from "../containers/MenuPlan";
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/RecipeBook" exact component={RecipeBook} />
+    <Route path="/MenuPlan" exact component={MenuPlan} />
+
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
