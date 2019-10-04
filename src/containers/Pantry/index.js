d0df7657c577f667aa43aa0e98d6693df285d14b
DMALCOL <daniel.malcolm@daimler.com>
2019-10-04 16:21:29 +0100

    added pantry container

---
+++ src/containers/Pantry/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return(
+    <div className="Pantry">
+      <h1>Pantry</h1>
+    </div>
+  )
+}
--- src/utils/Routes.js
+++ src/utils/Routes.js
@@ -4,13 +4,14 @@ import NotFound from '../containers/NotFound';
 import Home from "../containers/Home";
 import RecipeBook from "../containers/RecipeBook";
 import MenuPlan from "../containers/MenuPlan";
+import Pantry from "../containers/Pantry";
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/RecipeBook" exact component={RecipeBook} />
     <Route path="/MenuPlan" exact component={MenuPlan} />
-
+    <Route path="/Pantry" exact component={Pantry} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
