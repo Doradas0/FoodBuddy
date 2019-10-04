c960106f87837f7d45691f0d78aa56e771bf3f5f
DMALCOL <daniel.malcolm@daimler.com>
2019-10-04 16:15:01 +0100

    Added RecipeBook container

---
+++ src/containers/RecipeBook/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return(
+    <div className="RecipeBook">
+      <h1>Recipe Book</h1>
+    </div>
+  )
+}
--- src/utils/Routes.js
+++ src/utils/Routes.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import NotFound from '../containers/NotFound';
 import Home from "../containers/Home";
+import RecipeBook from "../containers/RecipeBook";
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
-
+    <Route path="/RecipeBook" exact component={RecipeBook} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
