364afbc610043cc8bb1344ec21e30003084f62a9
Daniel Malcolm <daniel.malcolm@daimler.com>
2019-10-17 19:32:40 +0100

    recipe-create

---
+++ backend/food-buddy-api/services/recipe-api/create.js
@@ -0,0 +1,31 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
+
+export async function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+  console.log(data);
+  const params = {
+    TableName: "dev-recipes",
+
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      recipeId: uuid.v1(),
+      name: data.name,
+      ingredients: data.ingredients,
+      method: data.method,
+      servings: data.servings,
+      time:data.time,
+      tags: data.tags,
+      attachment: data.attachment,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
