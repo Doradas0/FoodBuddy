78e988c0ae82cacb17fbf4eea52da4845ff25227
DMALCOL <daniel.malcolm@daimler.com>
2019-11-13 22:32:49 +0000

    React Material UI init

---
+++ frontend/food-buddy-client/src/Components/App.js
@@ -0,0 +1,7 @@
+import React, {Fragment} from 'react';
+import Header from "./Header";
+
+export default props =>
+  <Fragment>
+    <Header children="Test"/>
+  </Fragment>
---
+++ frontend/food-buddy-client/src/Components/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { AppBar, Toolbar, IconButton} from "@material-ui/core";
+import Link from '@material-ui/core/Link';
+import MenuIcon from "@material-ui/icons/Menu";
+import Button from '@material-ui/core/Button';
+
+export default props => {
+  return (
+    <AppBar position="static">
+    <Toolbar>
+      <IconButton edge="start" color="inherit" aria-label="menu">
+        <MenuIcon />
+      </IconButton>
+      {!props.authenticated
+        ?<UnauthenticatedToolBar />
+        :<AuthenticatedToolBar />
+      }
+    </Toolbar>
+    </AppBar>
+  );
+}
+
+const preventDefault = event => event.preventDefault();
+
+const UnauthenticatedToolBar= props => {
+  return(
+    <Button variant="contained">
+      <Link  onClick={preventDefault} to="/Login">Login</Link>
+    </Button>
+  );
+}
+
+const AuthenticatedToolBar= props => {
+  return(
+    "Au"
+  );
+}
