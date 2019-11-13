4de80f746abbea7d2f237a625afe1765e70d2fbe
DMALCOL <daniel.malcolm@daimler.com>
2019-11-13 20:04:24 +0000

    Delete old client

--- frontend/food-buddy-client/src/components/NewButton.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./NewButton.css";
-export default function NewButton({onClick, ...props}) {
-  return(
-    <button className="NewButton" onClick={onClick}>+</button>
-  )
-}
