4b1e15fa57ca159094052a05ee8fa9938fc6fb91
Daniel Malcolm <daniel.malcolm@daimler.com>
2019-12-04 16:36:34 +0000

    All fields are editablex

--- frontend/food-buddy-client/src/Components/RecipeCard.js
+++ frontend/food-buddy-client/src/Components/RecipeCard.js
@@ -4,7 +4,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
@@ -19,9 +18,16 @@ const useStyles = makeStyles(theme=>({
   smCard: {
     maxWidth: 345,
   },
+  smContent: {
+    display: "none"
+  },
   card:{
     maxWidth: theme.breakpoints.values.sm,
     position: "relative"
+  },
+  media: {
+    height: theme.spacing(30),
+    opacity: 0.5,
   },
   basicInfoContainer: {
     textTransform: "capitalize",
@@ -33,26 +39,42 @@ const useStyles = makeStyles(theme=>({
     color: theme.palette.primary.contrastText
   },
   basicInfo: {
+    margin: "0 6px",
+    padding: "4px 0",
+    color: theme.palette.primary.contrastText
+  },
+  basicInfoField:{
     display: "flex",
     alignItems:"center",
-    margin: "0 6px",
-    padding: "4px 0"
-  },
-  media: {
-    height: theme.spacing(30),
-    opacity: 0.5,
-  },
-  smContent: {
-    display: "none"
-  },
-  tabs: {
-  },
-  content: {
-    padding: 0
   },
   ingredientLine:{
     display: "flex",
+    borderBottom: `2px dotted ${theme.palette.secondary.main}`
   },
+  ingredientList: {
+    display: "flex",
+    flexDirection: "column",
+    padding: 0,
+    maxWidth: "500px",
+    margin: "auto"
+  },
+  ingredientQuantity: {
+    marginLeft: "auto",
+    width: "20px"
+  },
+  ingredientMeasurement: {
+    width: "60px"
+  },
+  methodList: {
+    display: "flex",
+    flexDirection: "column",
+    padding: 0,
+    maxWidth: "500px",
+    margin: "auto"
+  },
+  methodStep: {
+    borderBottom: `2px dotted ${theme.palette.secondary.main}`
+  }
 }));
 
 export default function RecipeCard({recipe, ...props}){
@@ -75,36 +97,12 @@ export default function RecipeCard({recipe, ...props}){
     let x = [...method];
     x[i] = e.target.value;
     setMethod(x);
-
   }
 
-  function MethodList({method}){
-    return method.map((step,i)=>(
-      <input
-        key={['step', i].join('_')}
-        onChange={(e)=>changemethod({e,i})}
-        value={step}
-      />
-    ));
-  }
-
-  function IngredientList({ingredients}){
-    return ingredients.map((ingredient,i)=>(
-      <div className={classes.ingredientLine} key={['ingredient', i].join('_')}>
-        <Typography component="p" variant="subtitle2" className={classes.ingredientItem}>
-          {ingredient.item}
-        </Typography>
-        <Typography component="p" variant="subtitle2" className={classes.ingredientQuantity}>
-          {ingredient.quantity}
-        </Typography>
-        <InputBase
-          key={i}
-          className={classes.ingredientMeasurement}
-          name="quantity"
-          value={ingredient.measurement}
-        />
-      </div>
-    ))
+  function changeIngredient({e,i}){
+    let x = [...ingredients];
+    x[i][e.target.name]=e.target.value;
+    setIngredients(x)
   }
 
   return(
@@ -123,44 +121,89 @@ export default function RecipeCard({recipe, ...props}){
           name="title"
           value={title}
         />
-        <div className={classes.basicInfo}>
+        <div className={classes.basicInfoField}>
           <LocalDiningIcon fontSize="small" color="secondary"/>
           <InputBase
+            className={classes.basicInfo}
             onChange={(e) => setServings(e.target.value)}
             name="servings"
             value={servings}
-            type="number"
           />
         </div>
-        <div className={classes.basicInfo}>
+        <div className={classes.basicInfoField}>
           <TimerIcon fontSize="small" color="secondary"/>
           <InputBase
+            className={classes.basicInfo}
             onChange={(e) => setCookTime(e.target.value)}
             name="cookTime"
             value={cookTime}
-            type="number"
           />
         </div>
       </div>
       <CardContent
         className={`${small && classes.smContent} ${classes.content}`}
       >
-      <Typography component="p" variant="body1">
-        Description
-      </Typography>
-
       <Tabs centered value={tabValue} onChange={handleTabChange} className={classes.tabs}>
         <Tab label="Ingredients"/>
         <Tab label="Method"/>
       </Tabs>
       <TabPanel value={tabValue} index={0}>
-        <IngredientList ingredients={ingredients} />
+        <IngredientList $ingredients={ingredients} changeIngredient={changeIngredient }/>
       </TabPanel>
       <TabPanel value={tabValue} index={1}>
-        <MethodList method={method}/>
+        <MethodList method={method} changemethod={changemethod}/>
       </TabPanel>
 
       </CardContent>
     </Card>
+  )
+}
+
+function MethodList({method,...props}){
+  const classes=useStyles();
+  const steps = method.map((step,i)=>(
+    <InputBase
+      key={['step', i].join('_')}
+      onChange={(e)=>props.changemethod({e,i})}
+      value={step}
+      className={classes.methodStep}
+    />
+  ));
+  return(
+    <ul className={classes.methodList}>
+      {steps}
+    </ul>
+  )
+}
+
+function IngredientList({$ingredients,...props}){
+  const classes=useStyles();
+  const ingredients = $ingredients.map((ingredient,i)=>(
+    <div className={classes.ingredientLine} key={['ingredient', i].join('_')}>
+      <InputBase
+        className={classes.ingredientItem}
+        onChange={(e)=>props.changeIngredient({e,i})}
+        name="item"
+        value={ingredient.item}
+      />
+      <InputBase
+        className={classes.ingredientQuantity}
+        onChange={(e)=>props.changeIngredient({e,i})}
+        name="quantity"
+        value={ingredient.quantity}
+
+      />
+      <InputBase
+        className={classes.ingredientMeasurement}
+        onChange={(e)=>props.changeIngredient({e,i})}
+        name="measurement"
+        value={ingredient.measurement}
+      />
+    </div>
+  ));
+  return(
+    <ul className={classes.ingredientList}>
+      {ingredients}
+    </ul>
   )
 }
